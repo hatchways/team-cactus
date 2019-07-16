ae520070ced32eb1927bb2d20f148815230e99be
merrilytan <merrilytan@gmail.com>
2019-07-16 14:18:28 -0400

    added passport authentication to POST /images

--- server/routes/images.js
+++ server/routes/images.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const upload = require('../services/image-upload');
-
 const singleUpload = upload.single('image');
+const passport = require("passport");
 
-router.post('/', function(req, res) {
+router.post('/', passport.authenticate('jwt', { session: false }), function(req, res) {
   singleUpload(req, res, function(err) {
     if (err) {
       return res.status(422).send({errors: [{title: 'Image Upload Error', detail: err.message}]});

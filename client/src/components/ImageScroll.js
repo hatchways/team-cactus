f3ff93efac0bcb7bdbbefc2190bb187fda803239
merrilytan <merrilytan@gmail.com>
2019-07-19 14:58:11 -0400

    added functionality to select product image to display

--- client/src/components/ImageScroll.js
+++ client/src/components/ImageScroll.js
@@ -17,15 +17,34 @@ const styles = theme => ({
     },
     imageScroll__image: {
         marginBottom: '10px',
-        width: '100%'
+        width: '100%',
+        opacity: 0.55,
+        cursor: 'pointer',
+        '&:hover': {
+            opacity: 1
+        }
     },
     imageMain: {
         width: '75%',
         paddingRight: '80px',
     },
+    currentImage: {
+        marginBottom: '10px',
+        width: '100%',
+        opacity: 1,
+        cursor: 'pointer',
+    }
 });
 
 class ImageScroll extends Component {
+
+    state = {
+        currentImage: 0
+    }
+
+    imageClick = (id) => {
+        this.setState({ currentImage: id});
+    }
 
     render() {
         const { classes, images } = this.props;
@@ -35,12 +54,16 @@ class ImageScroll extends Component {
             <div className={classes.imageContainer}>
                 <div className={classes.imageScroll}>
                     {images.forEach((image, index) => {
-                        printImages.push(<img src={image.URL} className={classes.imageScroll__image} key={image._id ? image._id : index} alt="jacket"/>);
+                        let imageClasses = classes.imageScroll__image;
+                        if(index===this.state.currentImage) {
+                            imageClasses = classes.currentImage;
+                        }
+                        printImages.push(<img id={index} src={image.URL} onClick={()=>this.imageClick(index)} className={imageClasses} key={image._id ? image._id : index} alt="jacket"/>);
                     })}
                     {printImages}
                 </div>
                 <div className={classes.imageMain}>
-                    <img src={images[0].URL} className={classes.imageScroll__image} key={images[0]._id ? images[0]._id : 1} width="100%" alt="jacket" /> 
+                    <img src={images[this.state.currentImage].URL} key={images[this.state.currentImage]._id ? images[this.state.currentImage]._id : 1} width="100%" alt="jacket" /> 
                 </div>
             </div>
         );

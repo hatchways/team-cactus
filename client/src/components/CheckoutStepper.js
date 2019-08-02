ebeda798289447d175f4d3e67f9ecad76584c2a5
Dina <dina400@hotmail.ca>
2019-08-02 17:03:36 -0400

    Stripe test works, moved checkout page content to Checkout file, cleaned UI

--- client/src/components/CheckoutStepper.js
+++ client/src/components/CheckoutStepper.js
@@ -1,118 +1,122 @@
-import React, { Component }from 'react';
+import React, { Component } from "react";
 import { withStyles } from "@material-ui/core/styles";
-import Stepper from '@material-ui/core/Stepper';
-import Step from '@material-ui/core/Step';
-import StepLabel from '@material-ui/core/StepLabel';
-// import Button from '@material-ui/core/Button';
-// import Typography from "@material-ui/core/Typography";
-import CheckoutForm from './Pages/PageComponents/CheckoutForm';
-import ShippingForm from './Pages/PageComponents/ShippingForm';
-import InformationForm from './Pages/PageComponents/CustomerInfoForm';
-import Divider from "@material-ui/core/Divider";
-import { Elements, StripeProvider } from 'react-stripe-elements';
-
-
-const STRIPE_PK = "pk_test_gwivf5Iq9bKkDQjzqDs7lFdj00SezimkV7";
+import Stepper from "@material-ui/core/Stepper";
+import Step from "@material-ui/core/Step";
+import StepLabel from "@material-ui/core/StepLabel";
 
 const styles = theme => ({
-  	formContainer: {
-  		margin: '30px 40px'
-  	},
-  	button: {
-    	boxShadow: 'none',
-    	borderRadius: 0,
-    	padding: '15px 20px',
-    	margin: '60px 0px',
-    	width: '30%',
-    },
+	step: {
+		[theme.breakpoints.down('sm')]: {
+			display: 'none'
+		}
+	}
 });
 
+// const QontoConnector = withStyles({
+// 	alternativeLabel: {
+// 		top: 10,
+// 		left: "calc(-50% + 16px)",
+// 		right: "calc(50% + 16px)"
+// 	},
+// 	active: {
+// 		"& $line": {
+// 			borderColor: "black"
+// 		}
+// 	},
+// 	completed: {
+// 		"& $line": {
+// 			borderColor: "black"
+// 		}
+// 	},
+// 	disabled: {
+// 		"& $line": {
+// 			borderColor: "grey"
+// 		}
+// 	},
+// 	line: {
+// 		borderTopWidth: 3,
+// 		borderRadius: 1
+// 	}
+// })(StepConnector);
+
+// const useQontoStepIconStyles = makeStyles({
+// 	root: {
+// 		color: "#eaeaf0",
+// 		display: "flex",
+// 		height: 22,
+// 		alignItems: "center"
+// 	},
+// 	active: {
+// 		color: "#784af4"
+// 	},
+// 	circle: {
+// 		width: 8,
+// 		height: 8,
+// 		borderRadius: "50%",
+// 		backgroundColor: "currentColor"
+// 	},
+// 	completed: {
+// 		color: "#784af4",
+// 		zIndex: 1,
+// 		fontSize: 18
+// 	}
+// });
+
+// function QontoStepIcon(props) {
+// 	const classes = useQontoStepIconStyles();
+// 	const { active, completed } = props;
+
+// 	return (
+// 		<div
+// 			className={clsx(classes.root, {
+// 				[classes.active]: active
+// 			})}
+// 		>
+// 			{completed ? (
+// 				<Check className={classes.completed} />
+// 			) : (
+// 				<div className={classes.circle} />
+// 			)}
+// 		</div>
+// 	);
+// }
+
+// QontoStepIcon.propTypes = {
+// 	active: PropTypes.bool,
+// 	completed: PropTypes.bool
+// };
+
+// const useStyles = makeStyles(theme => ({
+// 	root: {
+// 		width: "90%"
+// 	},
+// 	button: {
+// 		marginRight: theme.spacing(1)
+// 	},
+// 	instructions: {
+// 		marginTop: theme.spacing(1),
+// 		marginBottom: theme.spacing(1)
+// 	}
+// }));
+
 class CheckoutStepper extends Component {
-	constructor(props) {
-		super(props);
-
-		this.state = {
-			steps: ['Information', 'Shipping', 'Payment'],
-			activeStep: 2,
-		}
-	}
-
-	getActiveTab = () => {
-		switch (this.state.activeStep) {
-		    case 0:
-		    	return (
-		    		<InformationForm 
-	    				setEmail={ (email) => {this.setState( {email: email} )} }
-						handleNext={this.handleNext}
-		    		/>
-		    	);
-
-		    case 1:
-		    	return (
-		    		<ShippingForm 
-		    			email={this.state.email} 
-		    			handleBack={this.handleBack}
-		    			handleNext={this.handleNext}
-					/>
-				);
-
-		    case 2:
-		    	return (	
-		    		<StripeProvider apiKey={STRIPE_PK}>
-						<Elements>
-							<CheckoutForm 
-								email={this.state.email} 
-				    			handleBack={this.handleBack} 
-								handlePayment={() => {}}
-								purchaseData={this.props.purchaseData}
-							/>
-						</Elements>
-					</StripeProvider>
-				);
-
-		    default:
-		    	return 'Unknown step';
-		}
-	}
-
-	handleNext = () => {
-		let newStep = this.state.activeStep + 1;
-		if (newStep < this.state.steps.length) {
-			this.setState({activeStep: newStep})
-		}
-	}
-
-	handleBack = () => {
-		let newStep = this.state.activeStep - 1;
-		if (newStep >= 0) {
-			this.setState({activeStep: newStep})
-		}
-	}
-
 	render() {
-
 		const { classes } = this.props;
+			
 		return (
 			<div>
-	      		<Stepper activeStep={this.state.activeStep}>
-	        		{this.state.steps.map((label, index) => {
+				<Stepper activeStep={this.props.currentStep}>
+					{this.props.steps.map((label, index) => {
 						return (
-							<Step key={label}>
+							<Step key={label} className={classes.step}>
 								<StepLabel>{label}</StepLabel>
 							</Step>
 						);
-	        		})}
-	      		</Stepper>
-
-				<Divider/>
-
-				<div className={classes.formContainer}>
-					{this.getActiveTab()}
-				</div>
+					})}
+				</Stepper>
 			</div>
-	    );
+		);
 	}
 }
-
 
 export default withStyles(styles)(CheckoutStepper);
--- client/src/components/Pages/Checkout.js
+++ client/src/components/Pages/Checkout.js
@@ -8,7 +8,12 @@ import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
 import Divider from "@material-ui/core/Divider";
 import Grid from "@material-ui/core/Grid";
+import CheckoutForm from './PageComponents/CheckoutForm';
+import ShippingForm from './PageComponents/ShippingForm';
+import InformationForm from './PageComponents/CustomerInfoForm';
+import { Elements, StripeProvider } from 'react-stripe-elements';
 
+const STRIPE_PK = "pk_test_gwivf5Iq9bKkDQjzqDs7lFdj00SezimkV7";
 
 const styles = theme => ({
  	container: {
@@ -16,21 +21,31 @@ const styles = theme => ({
  		padding: '60px'
  	},
   	title: {
-        margin: '20px 40px',
+        // margin: '20px 40px',
         fontWeight: 600,
         letterSpacing: '1px',
         textDecoration: 'none',
         fontSize: '25px',
   	},
-  	checkout: {
-		// display: 'inline-block',
-		padding: '40px 0px',
+  	checkoutHeader: {
+  		width: '100%',
+  		display: 'flex',
+  		flexDirection: "row",
+  		justifyContent:"space-between",
+		alignItems: "center",
+		// display: 'flex',
+		// flexDirection: 'row',
+		// justifyContent: 'space-between'
+		padding: '20px 40px'
   	},
   	summary: {
   		borderTop: '7px solid black',
 	    justifyContent: "center",
 	    alignItems: 'flex-start'
   	},
+  	formContainer: {
+  		padding: '40px'
+  	}
   	// button: {
    //  	boxShadow: 'none',
    //  	borderRadius: 0,
@@ -46,9 +61,64 @@ class CheckoutPage extends Component {
 		super(props);
 
 		this.state = {
-			activeStep: 0,// "info", "shipping", "payment"
 			email: "",
+			steps: ['Information', 'Shipping', 'Payment'],
+			activeStep: 2
 		}
+	}
+
+	handleNext = () => {
+		let newStep = this.state.activeStep + 1;
+		if (newStep < this.state.steps.length) {
+			this.setState({activeStep: newStep})
+		}
+	}
+
+	handleBack = () => {
+		let newStep = this.state.activeStep - 1;
+		if (newStep >= 0) {
+			this.setState({activeStep: newStep})
+		}
+	}
+
+	getActiveTab = () => {
+
+		switch (this.state.activeStep) {
+		    case 0:
+		    	return (
+		    		<InformationForm 
+	    				setEmail={ (email) => {this.setState( {email: email} )} }
+						handleNext={this.handleNext}
+		    		/>
+		    	);
+
+		    case 1:
+		    	return (
+		    		<ShippingForm 
+		    			email={this.state.email} 
+		    			handleBack={this.handleBack}
+		    			handleNext={this.handleNext}
+					/>
+				);
+
+		    case 2:
+		    	return (	
+		    		<StripeProvider apiKey={STRIPE_PK}>
+						<Elements>
+							<CheckoutForm 
+								email={this.state.email} 
+				    			handleBack={this.handleBack} 
+								handlePayment={() => {}}
+								purchaseData={this.props.purchaseData}
+							/>
+						</Elements>
+					</StripeProvider>
+				);
+
+		    default:
+		    	return'Unknown step';
+		}
+	
 	}
 
 	render() {
@@ -61,16 +131,28 @@ class CheckoutPage extends Component {
 		return (
 			<PageWrapper>
 				<Grid container direction="row" alignItems="flex-start" justify="center" spacing={3}>
-					<Grid item md={8}>
-						<Paper square={true} >
-							<div className={classes.checkout}>
-								<Typography component="span" className={classes.title}> Checkout </Typography>
-								<CheckoutStepper purchaseData={purchaseData} />
+					<Grid item xs={8}>
+						<Paper square={true}>
+							<div className={classes.checkoutHeader}>
+								<div>
+									<Typography component="span" className={classes.title}> Checkout </Typography>
+								</div>
+								<div>
+									<CheckoutStepper 
+										purchaseData={purchaseData} 
+										currentStep={this.state.activeStep}
+										steps={this.state.steps}
+									/>
+								</div>
+							</div>
+							<Divider/>
+							<div className={classes.formContainer}>
+								{this.getActiveTab()}
 							</div>
 						</Paper>
 					</Grid>
 
-					<Grid item md={4}>
+					<Grid item xs={4}>
 						<Paper className={classes.summary} square={true}>
 							<div style={{height: '70%', margin: '0px 0px 40px 0px'}}>
 								<Typography className={classes.title} align="center"> Your Order: </Typography>
@@ -82,7 +164,7 @@ class CheckoutPage extends Component {
 								</Typography>
 							</div>
 							<Divider />
-							<div style={{display: 'inline-block', textAlign: 'center', margin: '10px'}}>
+							<div style={{width: '100%', display: 'inline-block', textAlign: 'center', margin: '10px'}}>
 								<Typography className={classes.title} component="span" align="center">
 									Total:
 								</Typography> 
